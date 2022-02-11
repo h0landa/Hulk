c5b7e4ecea8d2ac89f4d5506bdecacd1aa24c63a
h0landa <holandaarthur46@gmail.com>
2022-02-11 15:45:43 -0300

    Criando Interface

---
+++ .qt_for_python/uic/hulk.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'hulk.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        if not Dialog.objectName():
+            Dialog.setObjectName(u"Dialog")
+        Dialog.resize(477, 300)
+        Dialog.setStyleSheet(u"background-color: rgb(158, 255, 55);")
+        self.verticalLayoutWidget = QWidget(Dialog)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(10, 90, 461, 111))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.pushButton = QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.verticalLayout.addWidget(self.pushButton)
+
+
+        self.retranslateUi(Dialog)
+
+        QMetaObject.connectSlotsByName(Dialog)
+    # setupUi
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Hulk", None))
+        self.pushButton.setText(QCoreApplication.translate("Dialog", u"BUSCAR", None))
+    # retranslateUi
+
